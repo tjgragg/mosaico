57d27e841c6887e30130f9f7071c6120b4664196
tim.gragg <tim.gragg@asurion.com>
2020-08-27 11:41:19 -0500

    create new baseline by removing pieces of code we will not be using

--- src/js/ext/localstorage.js
+++ src/js/ext/localstorage.js
@@ -40,48 +40,9 @@ var lsCommandPluginFactory = function(md, emailProcessorBackend) {
       global.localStorage.setItem("template-" + mdkey, viewModel.exportJSON());
       saveCmd.enabled(true);
     };
-    var testCmd = {
-      name: 'Test', // l10n happens in the template
-      enabled: ko.observable(true)
-    };
     var downloadCmd = {
       name: 'Download', // l10n happens in the template
       enabled: ko.observable(true)
-    };
-    testCmd.execute = function() {
-      testCmd.enabled(false);
-      var email = global.localStorage.getItem("testemail");
-      if (email === null || email == 'null') email = viewModel.t('Insert here the recipient email address');
-      if (typeof global.prompt !== 'function') {
-        global.alert(viewModel.t('This feature is not supported by your browser'));
-        testCmd.enabled(true);
-      } else {
-        email = global.prompt(viewModel.t("Test email address"), email);
-        if (typeof email !== 'undefined' && email !== null && email.match(/@/)) {
-          global.localStorage.setItem("testemail", email);
-          var postUrl = emailProcessorBackend ? emailProcessorBackend : '/dl/';
-          var post = $.post(postUrl, {
-            action: 'email',
-            rcpt: email,
-            subject: "[test] " + mdkey + " - " + mdname,
-            html: viewModel.exportHTML()
-          }, null, 'html');
-          post.fail(function() {
-            console.log("fail", arguments);
-            viewModel.notifier.error(viewModel.t('Unexpected error talking to server: contact us!'));
-          });
-          post.success(function() {
-            console.log("success", arguments);
-            viewModel.notifier.success(viewModel.t("Test email sent..."));
-          });
-          post.always(function() {
-            testCmd.enabled(true);
-          });
-        } else {
-          global.alert(viewModel.t('Invalid email address'));
-          testCmd.enabled(true);
-        }
-      }
     };
     downloadCmd.execute = function() {
       downloadCmd.enabled(false);
@@ -94,7 +55,6 @@ var lsCommandPluginFactory = function(md, emailProcessorBackend) {
     };
 
     viewModel.save = saveCmd;
-    viewModel.test = testCmd;
     viewModel.download = downloadCmd;
   }.bind(undefined, md.key, md.name);
 
